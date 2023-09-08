6645bb90cc1afc9a4409bf497085b24e3e1da7b2
iqbalkang225 <iqbal.kang.cohort225@gmail.com>
2023-09-08 16:40:12 -0700

    first commit

---
+++ DependencyInjection/Services/CitiesService.cs
@@ -0,0 +1,23 @@
+﻿namespace Services
+{
+    public class CitiesService
+    {
+        private List<string> _cities;
+
+        public CitiesService()
+        {
+            _cities = new List<string>()
+            {
+                "Paris", "London", "Delhi", "Sacramento", "Auckland"
+            };
+        }
+
+        public List<string> GetCities()
+        {
+            return _cities;
+        }
+
+        //public List<string> GetCities { get; }
+
+    }
+}
